0be367ae67dabd15f6c966eea5f03751b732e1b0
Konrad Mrożek <konrad.mrozek@gmail.com>
2012-10-15 11:21:42 +0200

    Change atlassian maven repository url to regular HTTP.

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :repositories [["atlassian" "https://maven.atlassian.com/repository/public"]]
+  :repositories [["atlassian" "http://maven.atlassian.com/repository/public"]]
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [com.atlassian.jira/jira-rest-java-client "0.6-m9"]]
   :plugins [[lein-swank "1.4.4"]]
